9e2fc4532b06a5c8aa61f740a00eb76765d3a8c5
yangxingchen <yangxingchen@daixiaomi.com>
2019-05-28 16:26:51 +0800

    feat: poem search

--- apps/poem/apiview.py
+++ apps/poem/apiview.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+
 
 from base.utils import CustomeLimitOffsetPagination
 
@@ -10,3 +12,7 @@ class PoemViewset(viewsets.ModelViewSet):
     queryset = Poem.objects.all()
     serializer_class = PoemSerializer
     pagination_class = CustomeLimitOffsetPagination
+    # 过滤，搜索，排序
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    # filter_class = ArticleFilter
+    search_fields = ('auth_name',)
